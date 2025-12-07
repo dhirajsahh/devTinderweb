478b4c969edc51059e93916897a0b6076f84ce60
dhirajsahh <dhirajmahato2019@gmail.com>
2025-12-07 20:22:09 +0545

    created routes for different page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+
+import Body from "./Body";
+import Login from "./Login";
+import Profiles from "./Profiles";
+
 function App() {
   return (
     <>
-      <h1>DevTinder</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profiles />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      {/* <Navbar /> */}
     </>
   );
 }
