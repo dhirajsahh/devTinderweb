257117dd514db7b8ca356fada4bd6f761f8a843c
dhirajsahh <dhirajmahato2019@gmail.com>
2025-12-08 07:29:30 +0545

    configured store

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,22 @@ import Navbar from "./Navbar";
 import Body from "./Body";
 import Login from "./Login";
 import Profiles from "./Profiles";
+import { Provider } from "react-redux";
+import store from "./utilis/appStore";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profiles />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-      {/* <Navbar /> */}
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profiles />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "./utilis/userSlice";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [emailId, setEmailId] = useState("martin123@gmail.com");
+  const [password, setPassword] = useState("Martin@123");
+  const dispatch = useDispatch();
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/login",
+        {
+          email: emailId,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(addUser(response.data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="flex justify-center my-5">
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body gap-5">
+          <h2 className="card-title justify-center text-2xl">Login!</h2>
+          <div>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend text-xl">Email ID</legend>
+              <input
+                type="text"
+                className="input"
+                placeholder="Email Id"
+                value={emailId}
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </fieldset>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend text-xl">Password</legend>
+              <input
+                type="text"
+                className="input"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
